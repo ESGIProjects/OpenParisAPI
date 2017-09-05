dce6c3e0f198f519961901d38d91dbaef2ff47c3
Jason Pierna <siilver777@me.com>
2017-09-05 10:59:21 +0200

    csv parser, mongo db test

--- index.js
+++ index.js
@@ -9,6 +9,26 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Mongoose
+var mongoose = require('mongoose');
+var Logement = mongoose.model('Logement', new mongoose.Schema({
+    id: Number,
+    name: String,
+    hostId: Number,
+    neighbourhood: String,
+    latitiude: Number,
+    longitude: Number,
+    roomType: String,
+    price: Number,
+    minNights: Number,
+    nbReviews: Number
+}));
+
+mongoose.connect(process.env.MONGODB_URI, function(error) {
+    if (error) console.error(error);
+    else console.log('mongo connected');
+});
+
 app.use('/', require('./routes')(port));
 
 app.listen(port, function() {
--- routes.js
+++ routes.js
@@ -1,9 +1,25 @@
 module.exports = function(port) {
     var express = require('express');
     var router = express.Router();
+    var csv = require('csv');
+    var fs = require('fs');
 
     router.get('/', function(req, res) {
-        console.log('Hello');
+
+        fs.readFile('listing.csv', 'utf8', function(err, data){
+            if (err) { return console.log(err); }
+
+          csv.parse(data, function(err, data){
+            csv.transform(data, function(data){
+              return data.map(function(value){return value.toUpperCase()});
+            }, function(err, data){
+                console.log(data);
+              /*csv.stringify(data, function(err, data){
+                process.stdout.write(data);
+            });*/
+            });
+          });
+      });
     });
 
     return router;
