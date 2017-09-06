68cd2832349b9e1fac41d5e9f4e308c87b76d2c8
Jason Pierna <siilver777@me.com>
2017-09-06 16:36:21 +0200

    handle_update nearly done (error 502 to fix) and search nearly done too

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mysql = require('mysql');
+var request = require('request');
+var cron = require('node-cron');
 
 var port = process.env.PORT || 8080;
 var app = express();
@@ -20,19 +22,77 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
-connection.query('SELECT * FROM categories ORDER BY id', function(error, results, fields) {
-    if (error) throw error;
-    results.forEach(function(element, index) {
-        console.log(element.name);
-    });
-});
+cron.schedule('0 0 2 * * *', handle_update);
 
 //connection.end();
 
 // Routes
-app.use('/', require('./routes')(mysql, connection));
+app.use('/', require('./routes')(express, mysql, connection));
+//handle_update();
 
 // Lancement
 app.listen(port, function() {
     console.log('Running on port ' + port);
 });
+
+function handle_update() {
+
+    // 1. get all categories
+    connection.query('SELECT id FROM categories ORDER BY id', function(error, results, fields) {
+        if (error) {
+            console.error(error);
+        }
+        else {
+            // 2. make array with categories
+            var categories = [];
+            results.forEach(function(element, index) {
+                categories[index] = element.id;
+            });
+
+            categories = [289, 300];
+
+            // 3. make forEach on categories array
+            categories.forEach(function(category, index) {
+                var url = 'https://api.paris.fr/api/data/1.1/Equipements/get_equipements/?token=be82901c787874a6a855f122e93245eb8955989ba2e172e4e16f4a3648462afe&cid=' + category + '&offset=0&limit=300';
+
+                console.log('category: ', category);
+
+                // 4. Make request to ParisAPI
+
+                request(url, function(error, response, body) {
+                    console.log('statusCode:', response && response.statusCode);
+                    if (error) {
+                        console.error(error);
+                    } else {
+                        var json = JSON.parse(body);
+                        var data = json['data'];
+
+                        data.forEach(function(place, index) {
+                            // 5. Vérif de l'existence
+                            connection.query(mysql.format('SELECT * FROM places WHERE id = ?', [place['id']]), function(error, results, fields) {
+                                // 6. Ajout seulement si nouvelle entrée
+                                if (results.length == 0) {
+                                    var entry = {
+                                        'id': place['id'],
+                                        'name': place['name'],
+                                        'address': place['address'],
+                                        'zipCode': place['zipCode'],
+                                        'latitiude': place['lat'],
+                                        'longitude': place['lon'],
+                                        'cat_id': category,
+                                        'user_id': null
+                                    };
+
+                                    connection.query(mysql.format('INSERT INTO places VALUES(?, ?, ?, ?, ?, ?, ?, ?)', [entry['id'], entry['name'], entry['address'], entry['zipCode'], entry['latitiude'], entry['longitude'], entry['cat_id'], entry['user_id']]), function(error, results, fields) {
+                                        if (error) console.error(error);
+                                        else console.log(results.insertId);
+                                    });
+                                }
+                            });
+                        });
+                    }
+                });
+            });
+        }
+    });
+}
--- routes.js
+++ routes.js
@@ -1,6 +1,65 @@
-module.exports = function(mysql, connection) {
-    var express = require('express');
+module.exports = function(express, mysql, connection) {
     var router = express.Router();
+
+    router.post('/login', function(req, res) {
+
+    });
+
+    router.post('/signup', function(req, res) {
+
+    });
+
+    router.get('/search', function(req, res) {
+
+        console.log(req.query.priceMin);
+
+        if (req.query.priceMin === undefined) {
+            res.sendStatus(400);
+            return;
+        }
+
+        if (req.query.priceMax === undefined) {
+            res.sendStatus(400);
+            return;
+        }
+
+        if (req.query.duration === undefined) {
+            res.sendStatus(400);
+            return;
+        }
+
+        if (req.query.neighbourhood === undefined) {
+            res.sendStatus(400);
+            return;
+        }
+
+        connection.query(mysql.format('SELECT * FROM logements WHERE price >= ? AND price <= ? AND minNights <= ? AND neighbourhood = ?', [req.query.priceMin, req.query.priceMax, req.query.duration, req.query.neighbourhood]), function(error, results, fields) {
+            res.status(200).send(results);
+        });
+
+    });
+
+    router.get('/places', function(req, res) {
+
+    });
+
+    router.post('/places', function(req, res) {
+
+    });
+
+    router.get('/update', function(req, res) {
+        var url = "https://api.paris.fr/api/data/1.0/Equipements/get_categories/?token=be82901c787874a6a855f122e93245eb8955989ba2e172e4e16f4a3648462afe"
+
+        request(url, function (error, response, body) {
+          console.log('statusCode:', response && response.statusCode);
+          if (error) console.log('error:', error);
+          else {
+              console.log('body:', body);
+          }
+        });
+
+        res.status(200).send("");
+    });
 
     router.get('/parser', function(req, res) {
         var csv = require('csv');
